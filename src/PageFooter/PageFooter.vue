38d3e6c1e5ed766ebad72fa0ca1a46882f109369
Mathéo <matheo.raveux@gmail.com>
2022-02-07 14:24:16 +0100

    ToVue3: PageFooter.vue
    Signed-off-by: Mathéo <matheo.raveux@gmail.com>

--- src/PageFooter/PageFooter.vue
+++ src/PageFooter/PageFooter.vue
@@ -5,10 +5,7 @@
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-
-    @Component
-    export default class PageFooter extends Vue {}
+export default {}
 </script>
 
 <style scoped>
