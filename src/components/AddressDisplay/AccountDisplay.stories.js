e2a158d58be1e92b8ef4f4283db97a04c9a92159
Mathéo <matheo.raveux@gmail.com>
2022-03-14 10:40:31 -0600

    Storybook: Ass AddressDisplay Story
    Signed-off-by: Mathéo <matheo.raveux@gmail.com>

---
+++ src/components/AddressDisplay/AccountDisplay.stories.js
@@ -0,0 +1,36 @@
+import { action } from '@storybook/addon-actions';
+import AddressDisplay from './AddressDisplay.vue';
+
+export default {
+    title: 'AddressDisplay',
+    component: AddressDisplay,
+    argTypes: {
+        address: { control: { type: 'text' } },
+        copyable: { control: { type: 'boolean' } },
+    },
+};
+
+const Template = (args) => ({
+    // Components used in your story `template` are defined in the `components` object
+    components: { AddressDisplay },
+    // The story's `args` need to be mapped into the template through the `setup()` method
+    setup() {
+        // Story args can be spread into the returned object
+        return { ...args, action };
+    },
+    // Then, the spread values can be accessed directly in the template
+    template: `
+        <AddressDisplay
+            style="margin-top: 7rem; margin-left: 2rem;"
+
+            :address="address"
+            :copyable="copyable"
+        />
+    `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    address: 'NQ12 3ASK LDJF ALKS DJFA KLSD FJAK LSDJ FDRE',
+    copyable: false,
+};
