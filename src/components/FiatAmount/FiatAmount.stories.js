8c7ed386cd36a832afa2ae2a9fef296a9c198c8d
Mathéo <matheo.raveux@gmail.com>
2022-08-10 19:50:02 +0200

    Storybook: Add FiatAmount Story
    Signed-off-by: Mathéo <matheo.raveux@gmail.com>

---
+++ src/components/FiatAmount/FiatAmount.stories.js
@@ -0,0 +1,43 @@
+import FiatAmount from './FiatAmount.vue';
+
+export default {
+    title: 'FiatAmount',
+    component: FiatAmount,
+    argTypes: {
+        amount: { control: 'number' },
+        currency: { control: 'text' },
+        maxRelativeDeviation: { control: 'number' },
+        hideDecimals: { control: 'boolean' },
+        locale: { control: 'text' },
+    },
+};
+
+const Template = (args) => ({
+    // Components used in your story `template` are defined in the `components` object
+    components: { FiatAmount },
+    // The story's `args` need to be mapped into the template through the `setup()` method
+    setup() {
+        // Story args can be spread into the returned object
+        return { ...args };
+    },
+    // Then, the spread values can be accessed directly in the template
+    template: `
+        <div>
+            <FiatAmount
+                :amount="amount"
+                :currency="currency"
+                :maxRelativeDeviation="maxRelativeDeviation"
+                :locale="locale"
+                :hideDecimals="hideDecimals" />
+        </div>
+    `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    amount: 12345.67,
+    currency: 'eur',
+    maxRelativeDeviation: .1,
+    locale: navigator.language,
+    hideDecimals: false,
+};
