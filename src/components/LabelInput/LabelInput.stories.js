4ca0f9b80b3dcd69dab9fdeca233393108222a74
Mathéo <matheo.raveux@gmail.com>
2022-08-10 20:10:00 +0200

    Storybook: Add LabelInput Story
    Signed-off-by: Mathéo <matheo.raveux@gmail.com>

---
+++ src/components/LabelInput/LabelInput.stories.js
@@ -0,0 +1,45 @@
+import { action } from '@storybook/addon-actions';
+import LabelInput from './LabelInput.vue';
+
+export default {
+    title: 'LabelInput',
+    component: LabelInput,
+    argTypes: {
+        modelValue: { control: 'text', table: { disable: true } },
+        maxBytes: { control: 'number' },
+        placeholder: { control: 'text' },
+        vanishing: { control: 'boolean' },
+        disabled: { control: 'boolean' },
+    },
+};
+
+const Template = (args) => ({
+    // Components used in your story `template` are defined in the `components` object
+    components: { LabelInput },
+    // The story's `args` need to be mapped into the template through the `setup()` method
+    setup() {
+        // Story args can be spread into the returned object
+        return { ...args, action };
+    },
+    // Then, the spread values can be accessed directly in the template
+    template: `
+        <div>
+            <LabelInput
+                @changed="action('changed')($event)"
+                @input="action('input')($event.target.value)"
+                @paste="action('paste')($event.target.value)"
+                v-model="modelValue"
+                :maxBytes="maxBytes"
+                :placeholder="placeholder"
+                :vanishing="vanishing"
+                :disabled="disabled"
+            />
+        </div>
+    `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    disabled: false,
+    modelValue: '',
+};
