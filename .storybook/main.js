1143419ffad56eadde313c326c634403e65dfcd6
Mathéo <matheo.raveux@gmail.com>
2022-11-14 18:22:17 -0600

    Storybook: upgrade to last version
    Signed-off-by: Mathéo <matheo.raveux@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,27 @@
 const svgLoader = require("vite-svg-loader");
-
 module.exports = {
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-scss",
-    ],
-    features: {
-        postcss: false,
-    },
-    framework: "@storybook/vue3",
-    core: {
-        builder: "storybook-builder-vite"
-    },
-    async viteFinal(config) {
-        // customize the Vite config here
-        config.plugins.push(svgLoader({ svgo: false }))
-        // return the customized config
-        return config;
-    },
-}
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
+  features: {
+    postcss: false
+  },
+  framework: "@storybook/vue3",
+  core: {
+    builder: "@storybook/builder-vite"
+  },
+  async viteFinal(config) {
+    // customize the Vite config here
+    config.plugins.push(svgLoader({
+      svgo: false
+    }));
+    // return the customized config
+    return config;
+  }
+};
