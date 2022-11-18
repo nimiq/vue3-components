de4c9c483bdbee0cd84c5df63aa00eaf27b6fd79
Matheo <matheo.raveux@gmail.com>
2022-11-18 15:49:09 -0600

    Storybook: Finalize AccountRing story

--- src/components/AccountRing/AccountRing.stories.js
+++ src/components/AccountRing/AccountRing.stories.js
@@ -5,28 +5,25 @@ export default {
     title: 'AccountRing',
     component: AccountRing,
     argTypes: {
+        // Props
         addresses: { control: { type: 'object' } },
         animate: { control: { type: 'boolean' } },
     },
 };
 
 const Template = (args) => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { AccountRing },
-    // The story's `args` need to be mapped into the template through the `setup()` method
     setup() {
-        // Story args can be spread into the returned object
         return { args, action };
     },
-    // Then, the spread values can be accessed directly in the template
     template: `
-        <div>
-            Atomatic width/height:<br/><br/>
-            <AccountRing v-bind="args"/>
-
-            <br/>300px width/height:<br/><br/>
-            <AccountRing style="width: 300px;" v-bind="args"/>
-        </div>
+        Atomatic width/height:
+        <br/><br/>
+        <AccountRing v-bind="args"/>
+        <br/>
+        300px width/height:
+        <br/><br/>
+        <AccountRing v-bind="args" style="width: 300px;"/>
     `,
 });
 
