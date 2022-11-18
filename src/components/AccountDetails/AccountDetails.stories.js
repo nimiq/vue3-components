67395ef48752c1d45d76ec3fc1cf8d497704f7de
Matheo <matheo.raveux@gmail.com>
2022-11-18 15:34:30 -0600

    Storybook: Finalize AccountDetails story

--- src/components/AccountDetails/AccountDetails.stories.js
+++ src/components/AccountDetails/AccountDetails.stories.js
@@ -1,11 +1,12 @@
-import { action } from '@storybook/addon-actions';
-import AccountDetails from './AccountDetails.vue';
-import { computed, ref } from 'vue';
+import AccountDetails, { AccountDetailsEvent } from './AccountDetails.vue';
+import { ref } from 'vue';
+import { getEventArgTypeFromEnum, getEventListenerFromEnum } from '../../helpers/storybook/EventHelper';
 
 export default {
     title: 'AccountDetails',
     component: AccountDetails,
     argTypes: {
+        // Props
         address: { control: { type: 'text' } },
         image: { control: { type: 'text' } },
         label: { control: { type: 'text' } },
@@ -13,13 +14,14 @@ export default {
         balance: { control: { type: 'number' } },
         editable: { control: { type: 'boolean' } },
         placeholder: { control: { type: 'text' } },
+
+        // Events
+        ...getEventArgTypeFromEnum(AccountDetailsEvent),
     },
 };
 
 const Template = (args) => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { AccountDetails },
-    // The story's `args` need to be mapped into the template through the `setup()` method
     setup() {
         const accountDetails$ = ref(null);
 
@@ -27,18 +29,18 @@ const Template = (args) => ({
             if (accountDetails$.value) accountDetails$.value.focus();
         }
 
-        // Story args can be spread into the returned object
-        return { args, accountDetails$, focus, action };
+        return {
+            events: getEventListenerFromEnum(AccountDetailsEvent),
+            args,
+            accountDetails$,
+            focus,
+        };
     },
-    // Then, the spread values can be accessed directly in the template
     template: `
         <button class="nq-button-s" @click="focus">Focus (editable: true)</button>
         <br />
         <br />
-        <AccountDetails ref="accountDetails$" v-bind="args"
-            @changed="action('changed')($event)"
-            @close="action('close')($event)"
-        />
+        <AccountDetails ref="accountDetails$" v-bind="args" v-on="events" />
     `,
 });
 
@@ -48,7 +50,6 @@ Default.args = {
     address: 'NQ33 DH76 PHUKJ41Q LX3A U4E0 M0BM QJH9 QQL1',
     label: 'Savings Account',
     balance: 2712415141213,
-    editable: false,
 };
 
 export const Merchant = Template.bind({});
@@ -57,5 +58,4 @@ Merchant.args = {
     origin: 'https://macces.com',
     // image: 'https://shop.nimiq.com/wp-content/uploads/2018/10/nimiq_signet_rgb_base_size.576px.png',
     image: 'https://pbs.twimg.com/profile_images/1150268408287698945/x4f3ITmx_400x400.png',
-    editable: false,
 };
