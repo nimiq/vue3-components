e988bf45ec6656f23bb67d69a70d2cfb13e3fb80
Matheo <matheo.raveux@gmail.com>
2022-11-19 16:45:23 -0600

    Storybook: Finalize Identicon story

--- src/components/Identicon/Identicon.stories.js
+++ src/components/Identicon/Identicon.stories.js
@@ -4,23 +4,18 @@ export default {
     title: 'Identicon',
     component: Identicon,
     argTypes: {
-        address: { control: 'text' },
+        // Props
+        address: { control: { type: 'text' } },
     },
 };
 
 const Template = (args) => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { Identicon },
-    // The story's `args` need to be mapped into the template through the `setup()` method
     setup() {
-        // Story args can be spread into the returned object
         return { args };
     },
-    // Then, the spread values can be accessed directly in the template
     template: `
-        <div>
-            <Identicon v-bind="args"/>
-        </div>
+        <Identicon v-bind="args"/>
     `,
 });
 
