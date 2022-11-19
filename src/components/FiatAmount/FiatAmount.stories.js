6f28a7b5fcee10a97552fa56e652b7e94a5505a3
Matheo <matheo.raveux@gmail.com>
2022-11-19 16:46:53 -0600

    Storybook: Finalize FiatAmount story

--- src/components/FiatAmount/FiatAmount.stories.js
+++ src/components/FiatAmount/FiatAmount.stories.js
@@ -4,27 +4,22 @@ export default {
     title: 'FiatAmount',
     component: FiatAmount,
     argTypes: {
-        amount: { control: 'number' },
-        currency: { control: 'text' },
-        maxRelativeDeviation: { control: 'number' },
-        hideDecimals: { control: 'boolean' },
-        locale: { control: 'text' },
+        // Props
+        amount: { control: { type: 'number' } },
+        currency: { control: { type: 'text' } },
+        maxRelativeDeviation: { control: { type: 'number' } },
+        hideDecimals: { control: { type: 'boolean' } },
+        locale: { control: { type: 'text' } },
     },
 };
 
 const Template = (args) => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { FiatAmount },
-    // The story's `args` need to be mapped into the template through the `setup()` method
     setup() {
-        // Story args can be spread into the returned object
         return { args };
     },
-    // Then, the spread values can be accessed directly in the template
     template: `
-        <div>
-            <FiatAmount v-bind="args" />
-        </div>
+        <FiatAmount v-bind="args" />
     `,
 });
 
@@ -34,5 +29,4 @@ Default.args = {
     currency: 'eur',
     maxRelativeDeviation: .1,
     locale: navigator.language,
-    hideDecimals: false,
 };
