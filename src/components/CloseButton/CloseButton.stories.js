a037eba98dd228df454ecc41ddbe80159ac8ca45
Matheo <matheo.raveux@gmail.com>
2022-11-19 16:25:01 -0600

    Storybook: Finalize CloseButton story

--- src/components/CloseButton/CloseButton.stories.js
+++ src/components/CloseButton/CloseButton.stories.js
@@ -7,18 +7,12 @@ export default {
 };
 
 const Template = (args) => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { CloseButton },
-    // The story's `args` need to be mapped into the template through the `setup()` method
     setup() {
-        // Story args can be spread into the returned object
         return { ...args, action };
     },
-    // Then, the spread values can be accessed directly in the template
     template: `
-        <div>
-            <CloseButton class="top-left" @click="action('click')($event)"/>
-        </div>
+        <CloseButton class="top-left" @click="action('click')($event)"/>
     `,
 });
 
