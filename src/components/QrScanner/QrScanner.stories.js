152bcc2d9a3e1d8c1a19bbc103c1197c2a7a8daa
Matheo <matheo.raveux@gmail.com>
2022-11-19 18:18:20 -0600

    Storybook: Finalize QrScanner story

--- src/components/QrScanner/QrScanner.stories.js
+++ src/components/QrScanner/QrScanner.stories.js
@@ -1,35 +1,35 @@
-import { action } from '@storybook/addon-actions';
-import QrScanner from './QrScanner.vue';
+import QrScanner, { QrScannerEvents } from './QrScanner.vue';
 import SmallPage from '../SmallPage/SmallPage.vue';
+import { getEventArgTypeFromEnum, getEventListenerFromEnum } from '../../helpers/storybook/EventHelper';
 
 export default {
     title: 'QrScanner',
     component: QrScanner,
     argTypes: {
+        // Props
         reportFrequency: { control: 'number' },
         validate: { table: { disable: true } },
+
+        // Slots
+        default: { control: false },
+
+        // Events
+        ...getEventArgTypeFromEnum(QrScannerEvents),
     },
 };
 
 const Template = (args) => ({
-    // Components used in your story `template` are defined in the `components` object
     components: { QrScanner, SmallPage },
-    // The story's `args` need to be mapped into the template through the `setup()` method
     setup() {
-        // Story args can be spread into the returned object
-        return { ...args, action };
+        return {
+            events: getEventListenerFromEnum(QrScannerEvents),
+            args,
+        };
     },
-    // Then, the spread values can be accessed directly in the template
     template: `
-        <div>
-            <SmallPage style="height: 560px; overflow: hidden">
-                <QrScanner style="height: 100%"
-                    @result="action('result')($event)"
-                    @cancel="action('cancel')($event)"
-                    @error="action('error')($event)"
-                ></QrScanner>
-            </SmallPage>
-        </div>
+        <SmallPage style="height: 560px; overflow: hidden">
+            <QrScanner style="height: 100%" v-bind="args" v-on="events"></QrScanner>
+        </SmallPage>
     `,
 });
 
