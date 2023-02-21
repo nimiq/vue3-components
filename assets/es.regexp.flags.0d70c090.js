8e6a261b7fcc6bf013474254a18689958583233d
Daniel <github.danimoh@neverbox.com>
2023-02-22 00:14:10 +0100

    Update storybook build

--- assets/es.regexp.flags.0d70c090.js
+++
@@ -1,2 +0,0 @@
-import{n as r,m as t,a4 as a,a as o}from"./iframe.303f0789.js";var s=r,p=t,g=a,l=o,e=RegExp.prototype,f=s&&l(function(){return Object.getOwnPropertyDescriptor(e,"flags").get.call({dotAll:!0,sticky:!0})!=="sy"});f&&p.f(e,"flags",{configurable:!0,get:g});
-//# sourceMappingURL=es.regexp.flags.0d70c090.js.map
